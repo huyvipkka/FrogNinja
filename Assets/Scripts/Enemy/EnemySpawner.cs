453f5e95ba8160972300f7245c64ae68f4b63da2
Huy <huypoilkj@gmail.com>
2024-04-16 11:33:09 +0700

    optimize enemy pools

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,24 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
-public class EnemyManager : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] List<ObjectPool<GameObject>> pools;
+    [SerializeField] GameObject[] enemyPrefabs;
+    [SerializeField] float spawnRaze = 1;
+    List<ObjectPool<GameObject>> pools;
+    float spawnTimer = 0;
     private void Awake()
     {
         pools = new List<ObjectPool<GameObject>>();
-        Transform parentTransform = gameObject.transform;
-        for (int i = 0; i < parentTransform.childCount; i++)
+        foreach (GameObject prefab in enemyPrefabs)
         {
-            Transform childTransform = parentTransform.GetChild(i);
-
-            ObjectPool<GameObject> pool = new(() => Instantiate(childTransform.gameObject),
+            ObjectPool<GameObject> pool = new(() => Instantiate(prefab),
                 ActionOnGet, ActionOnRelease, ActionOnDestroy,
                 collectionCheck: true, defaultCapacity: 100, maxSize: 1000);
             pools.Add(pool);
         }
-        Debug.Log(parentTransform.childCount);
-        Debug.Log(pools.Count);
     }
 
     private void ActionOnGet(GameObject obj)
@@ -27,6 +25,7 @@ public class EnemyManager : MonoBehaviour
         {
             enemy.ResetPos(transform.position);
         }
+        obj.transform.parent = transform;
         obj.SetActive(true);
     }
 
@@ -45,17 +44,13 @@ public class EnemyManager : MonoBehaviour
         SpawnEnemy();
     }
 
-    float spawnRaze = 1;
-    float spawnTimer = 0;
     void SpawnEnemy()
     {
         spawnTimer += Time.deltaTime;
         if (spawnTimer < spawnRaze) return;
         spawnTimer = 0;
 
-        spawnRaze = Random.Range(0.8f, 2);
         int index = Random.Range(0, pools.Count);
-
         GameObject enemyClone = pools[index].Get();
         if (enemyClone.TryGetComponent(out EnemyBase enemy))
         {
